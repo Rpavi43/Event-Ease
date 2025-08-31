f765502a000bc791a84ff2317c9990cb64d9f453
Rasuri Pavithra <pavithrareddy2104@gmail.com>
2025-08-31 16:44:26 +0530

    Update database configuration in config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-# config.py
-class config:
+import os
 
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Password@43'  # Use your MySQL password
-    MYSQL_DB = 'eventease'
-    SECRET_KEY = 'pavithra43'
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
